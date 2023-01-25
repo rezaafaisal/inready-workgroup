0c877f6b7565af74e29f895feec20b95d0b4dd11
rezaafaisal <rezha.faizal567@gmail.com>
2023-01-26 00:30:24 +0800

    add alpine js

--- resources/views/partials/user/header.blade.php
+++ resources/views/partials/user/header.blade.php
@@ -1,10 +1,28 @@
 <header class="bg-inr-black w-full">
     <div class="wrapper flex justify-between">
         <img src="{{ asset('images/ui/logo_yellow.png') }}" alt="logo" class="w-20 h-20">
-        <ul class="flex items-center gap-5 text-white">
-            <li>satu</li>
-            <li>satu</li>
-            <li>satu</li>
+        <ul class="flex items-center gap-7 text-white">
+            <li>
+                <a href="" class="hover:text-inr-yellow duration-150">Beranda</a>
+            </li>
+            <li>
+                <a href="" class="hover:text-inr-yellow duration-150">Profil</a>
+            </li>
+            <li>
+                <a href="" class="hover:text-inr-yellow duration-150">Kegiatan</a>
+            </li>
+            <li>
+                <a href="" class="hover:text-inr-yellow duration-150">Karya</a>
+            </li>
+            <li>
+                <a href="" class="hover:text-inr-yellow duration-150">Angkatan</a>
+            </li>
+            <li>
+                <a href="" class="hover:text-inr-yellow duration-150">Tentang Kami</a>
+            </li>
+            <li>
+                <button class="px-4 py-1 bg-inr-yellow text-white rounded-full">Masuk</button>
+            </li>
         </ul>
     </div>
 </header>
