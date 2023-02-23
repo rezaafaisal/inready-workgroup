9e30e4ae830aa09598c03cde364a6241acf0cd35
rezaafaisal <rezha.faizal567@gmail.com>
2023-02-24 00:07:32 +0800

    preview image

--- resources/views/user/profile/profile.blade.php
+++ resources/views/user/profile/profile.blade.php
@@ -1,29 +1,29 @@
 @extends('layouts.user')
 @section('styles')
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.css">
 @endsection
 @section('body')
-<div class="fixed z-50 flex justify-center items-center bg-inr-black bg-opacity-50 h-screen w-full overflow-hidden">
-    <div class="w-full m-10 md:w-8/12 lg:w-4/12 bg-inr-white rounded-lg">
-        <div class="p-4 border-b border-slate-300 flex justify-between">
-            <span>Sesuaikan Gambar</span>
-            <button><i class="fas fa-xmark"></i></button>
-        </div>
-        <div class="p-4">
-            <img src="{{ asset('images/ui/eren.jpeg') }}" class="croppie">
-        </div>
-        <div class="p-4 border-t border-slate-300 flex justify-end">
-            <div class="flex gap-3">
-                <button>Batal</button>
-                <button>Terapkan</button>
-            </div>
-        </div>
-    </div>
-</div>
 {{-- <script>
        document.documentElement.style.overflow = 'hidden';
 </script> --}}
-<section class="pt-20">
+<section x-data="{show:false}" class="pt-20">
+    <div x-show="show" class="fixed flex top-0 z-50 justify-center items-center bg-inr-black bg-opacity-50 h-screen w-full overflow-hidden">
+        <div class="w-full m-10 md:w-8/12 lg:w-4/12 bg-inr-white rounded-lg">
+            <div class="p-4 border-b border-slate-300 flex justify-between">
+                <span>Sesuaikan Gambar</span>
+                <button @click="show=false"><i class="fas fa-xmark"></i></button>
+            </div>
+            <div class="p-4">
+                <img src="" alt="" id="cropping" class="croppie">
+            </div>
+            <div class="p-4 border-t border-slate-300 flex justify-end">
+                <div class="flex gap-3 text-sm font-light">
+                    <button @click="show=false" class="rounded bg-gray-300 px-3 py-1">Batal</button>
+                    <button id="crop_image" class="rounded bg-inr-yellow px-3 py-1">Terapkan</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="wrapper py-20">
         <div class="flex">
             @include('partials.user.setting_sidebar')
@@ -32,19 +32,18 @@
                     <div class="group mb-7">
                         <span class="font-semibold block mb-2">Foto Diri</span>
                         <input type="file" x-ref="image" name="image" id="image" class="hidden"
-                            x-on:change="alert('halo')">
+                            x-on:change="show=true" onchange="readImg(this)">
                         <div class="flex gap-5">
-                            <img src="{{ asset('images/ui/eren.jpeg') }}" alt=""
+                            <img id="profile_image" src="{{ asset('images/ui/eren.jpeg') }}" alt=""
                                 class="w-28 h-28 object-cover rounded">
                             <div>
-                                <button @click="$refs.image.click()" type="button" class="btn-yellow text-sm">Pilih
-                                    Foto</button>
-                                <p class="font-extralight mt-4 text-sm max-w-md">Gambar Profile Anda sebaiknya memiliki
-                                    rasio 1:1
-                                    dan berukuran tidak lebih dari 2MB.</p>
+                            <button @click="$refs.image.click()" type="button" class="btn-yellow text-sm">Pilih
+                                Foto</button>
+                            <p class="font-extralight mt-4 text-sm max-w-md">Gambar Profile Anda sebaiknya memiliki
+                                rasio 1:1
+                                dan berukuran tidak lebih dari 2MB.</p>
                             </div>
                         </div>
-
                     </div>
                     <label for="" class="mb-7 block">
                         <span class="font-semibold block mb-2">Nama Lengkap <span class="text-rose-500">*</span></span>
@@ -73,21 +72,37 @@
 @section('scripts')
 <script src="https://code.jquery.com/jquery-3.6.3.min.js"
     integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-    
 <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.js"></script>
- 
 <script>
-    $('.croppie').croppie({
-    viewport: {
-        width: 250,
-        height: 250,
-        type:'square'
-    },
-    boundary:{
-        width: 300,
-        height: 300
-    }
-});
+    const preview = $('.croppie').croppie({
+        viewport: {
+            width: 250,
+            height: 250,
+            type: 'square'
+        },
+        boundary: {
+            width: 300,
+            height: 300
+        },
+    });
 
+    $('#crop_image').click(()=>{
+        preview.croppie('result', {type: 'base64', format: 'png'}).then((image) => {
+            $('#profile_image').attr('src', image)
+        })
+    })
+    
+    function readImg(e){
+        const file = e.files[0];
+        if(file){
+            let reader = new FileReader();
+            reader.onload = function(event){
+                preview.croppie('bind', {
+                    url: event.target.result
+                });
+            }
+            reader.readAsDataURL(file);
+        }
+    }
 </script>
 @endsection
