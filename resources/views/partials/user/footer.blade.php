04bbf25d90819bd2fcbd8d65468ba27427e26908
rezaafaisal <rezha.faizal567@gmail.com>
2023-03-17 17:26:21 +0800

    before on progress

--- resources/views/partials/user/footer.blade.php
+++ resources/views/partials/user/footer.blade.php
@@ -5,8 +5,10 @@
                 <img src="{{ asset('images/ui/logo_yellow.png') }}" alt="" class="w-12 h-12">
                 <h3 class="text-inr-yellow text-lg font-bold">Inready Workgroup</h3>
             </div>
-            <p class="text-sm text-justify font-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque deserunt tempore doloremque quos sunt, .</p>
-            <a href="" class="block mt-5 text-sm font-bold">
+            <p class="text-sm text-justify font-light">
+                Inready Workgroup adalah salah satu studi club external di lingkup UIN Alauddin Makassar, kunjungi sekretariat kami.
+            </p>
+            <a href="https://goo.gl/maps/U3pUFoZKY3g3aRME8" target="_blank" class="block mt-5 text-sm font-bold">
                 <i class="fas fa-map-pin mr-1 text-inr-yellow"></i>
                 Perumahan saumata indah
             </a>
@@ -15,13 +17,13 @@
             <h4 class="font-semibold text-lg mb-5">Kontak</h4>
             <ul class="text-sm flex flex-col gap-3 text-inr-white-1 font-light">
                 <li>
-                    <a href="">
+                    <a href="https://wa.me/6285341614066" target="_blank">
                         <i class="fas fa-phone w-5"></i>
-                        +62 8534216789
+                        +62 85341614066
                     </a>
                 </li>
                 <li>
-                    <a href="">
+                    <a href="mailto:inready.workgroup04@gmail.com" target="_blank">
                         <i class="fas fa-envelope w-5"></i>
                         inready.workgroup04@gmail.com
                     </a>
@@ -32,19 +34,19 @@
             <h4 class="font-semibold text-lg mb-5">Sosial Media</h4>
             <ul class="text-sm flex flex-col gap-3 text-inr-white-1 font-light">
                 <li>
-                    <a href="">
+                    <a href="https://instagram.com/inready.workgroup" target="_blank">
                         <i class="fab fa-instagram w-5"></i>
                         inready.workgroup
                     </a>
                 </li>
-                <li>
+                {{-- <li>
                     <a href="">
                         <i class="fab fa-youtube w-5"></i>
                         inready.workgroup04@gmail.com
                     </a>
-                </li>
+                </li> --}}
                 <li>
-                    <a href="">
+                    <a href="https://facebook.com/inreadyworkgroup" target="_blank">
                         <i class="fab fa-facebook w-5"></i>
                         Inready Workgroup
                     </a>
